77b5cf3739950e8cd18bfa2d464d741a7c502a99
GerardoASP <79153918+GerardoASP@users.noreply.github.com>
2021-02-16 17:19:49 -0500

    Add files via upload

--- Reto 1 TAD I/js/algoritmosOrdenamientos.js
+++ Reto 1 TAD I/js/algoritmosOrdenamientos.js
@@ -1,13 +1,61 @@
-function algoritmoBurbuja() {
-    alert("Soy la función burbuja");
+function algoritmoBurbuja(arr, dir=1) {
+    var noSwaps = true;
+  for(var i=arr.length; i>0; i--){
+    for(var j=0; j<i-1; j++){
+      if(dir===1){
+        if(arr[j]>arr[j+1]){
+          var temp=arr[j];
+          arr[j]=arr[j+1];
+          arr[j+1]=temp;
+          noSwaps=false;
+        }
+      }else{
+        if(arr[j]<arr[j+1]){
+          var temp=arr[j];
+          arr[j]=arr[j+1];
+          arr[j+1]=temp;
+          noSwaps=false;
+        }
+      }
+    }
+    if(noSwaps)
+      break;
+  }
+  return arr;
 }
 
-function algoritmoSeleccion() {
-    alert("Soy la función selección");
+function algoritmoSeleccion(arr, dir=1) {
+  const swap = (arr, idx1, idx2) => ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+  
+  for (let i = 0; i < arr.length; i++){
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++){
+      if(dir===1){
+        if (arr[lowest] > arr[j]){
+          lowest = j;
+        }
+      }else{
+        if (arr[lowest] < arr[j]){
+          lowest = j;
+        }
+      }
+    }
+    if (i !== lowest)
+      swap(arr, i, lowest);
+  }
+  return arr;
 }
 
-function algoritmoInsercion() {
-    alert("Soy la función inserción");
+function algoritmoInsercion(arr, dir=1) {
+  var currentVal;
+  for(var i = 1; i < arr.length; i++){
+    currentVal = arr[i];
+    for(var j = i - 1; j >= 0 && (dir===1)? arr[j] > currentVal:arr[j] < currentVal; j--){
+      arr[j+1] = arr[j];
+    }
+    arr[j+1] = currentVal;
+  }
+  return arr;
 }
 
 function showSelected() {
